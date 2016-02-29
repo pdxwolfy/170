deb6ddff1acc24d65384de73b06561adb5c267d4
Pete Hanson <wolfy@pdxwolfy.org>
2016-02-29 07:43:44 -0800

    Load table of contents in / slash

--- BookViewer/book_viewer.rb
+++ BookViewer/book_viewer.rb
@@ -2,7 +2,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+TOC = 'data/toc.txt'
+
 get '/' do
   @title = 'The Adventures of Sherlock Holmes'
+  @toc = File.read TOC
   erb :home
 end
