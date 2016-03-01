86e8fd8370d2889537ef1b6b3bb3315f23ca4266
Pete Hanson <wolfy@pdxwolfy.org>
2016-02-29 20:59:51 -0800

    Initial pass through program. Ready for CSS.

--- DDI/ddi.rb
+++ DDI/ddi.rb
@@ -5,9 +5,22 @@ require 'tilt/erubis'
 get '/' do
   @title = 'Dynamic Directory Index'
   @directory_name = File.join Dir.pwd, 'public'
-  @files = Dir.entries(@directory_name).select do |file_name|
-    File.file? File.join(@directory_name, file_name)
-  end
+  @sequence = params['sequence']
+
+  @files = select_files all_entries_in(@directory_name), @directory_name
+  @files.reverse! if @sequence == 'descending'
 
   erb :index
+end
+
+def all_entries_in(directory_name)
+  Dir.entries(directory_name).sort do |name_a, name_b|
+    name_a.downcase <=> name_b.downcase
+  end
+end
+
+def select_files(directory_entries, directory_name)
+  directory_entries.select do |file_name|
+    File.file? File.join(directory_name, file_name)
+  end
 end
