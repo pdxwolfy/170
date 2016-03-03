116daf35eed9ae19e42ee5c18be89a9548822cad
Pete Hanson <wolfy@pdxwolfy.org>
2016-03-02 23:31:27 -0800

    saving

--- BookViewer/book_viewer.rb
+++ BookViewer/book_viewer.rb
@@ -5,24 +5,60 @@ require 'tilt/erubis'
 TABLE_OF_CONTENTS = 'data/toc.txt'
 CHAPTER_TEXT = 'data/chp%{chapter_number}.txt'
 
-def initialize_common_variables
+before do
   @contents = File.readlines TABLE_OF_CONTENTS
 end
 
+helpers do
+  def emphasize(text, word)
+    word_re = Regexp.new(word, Regexp::IGNORECASE)
+    text.gsub(word_re, "<strong>#{word}</strong>")
+  end
+
+  def in_paragraphs(text)
+    paragraphs = text.split("\n\n").each_with_index.map do |paragraph, index|
+      "<p id='para_#{index}'>#{paragraph}</p>"
+    end
+    paragraphs.join
+  end
+end
+
+def load_chapter(chapter_number)
+  chapter_file = format CHAPTER_TEXT, chapter_number: chapter_number
+  File.read chapter_file, encoding: 'UTF-8'
+end
+
+def search(contents, query)
+  search_term = query.downcase
+  contents.each_with_index.each_with_object([]) do |chapter, found|
+    chapter_title, chapter_number = chapter
+    text = load_chapter(chapter_number + 1)
+    text.split("\n\n").each_with_index do |paragraph, paragraph_index|
+      next unless paragraph.downcase.include? search_term
+      found << [chapter_title, chapter_number, paragraph, paragraph_index]
+    end
+  end
+end
+
 get '/' do
-  initialize_common_variables
   @title = 'The Adventures of Sherlock Holmes'
+
   erb :home
 end
 
 get '/chapter/:chapter_number' do |chapter_number|
-  initialize_common_variables
   @title = "Chapter #{chapter_number}: #{@contents[chapter_number.to_i - 1]}"
-  chapter_file = format CHAPTER_TEXT, chapter_number: chapter_number
-  @paragraphs = File.readlines chapter_file, "\n\n"
+  @text = load_chapter chapter_number
+
   erb :chapter
 end
 
-get '/show/:name' do
-  params[:name]
+get '/search' do
+  @results = search @contents, params[:query] if params[:query]
+
+  erb :search
+end
+
+not_found do
+  redirect '/'
 end
