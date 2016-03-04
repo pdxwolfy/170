99bdc7867cb5dd12b6851096c85a37a1f3779170
Pete Hanson <wolfy@pdxwolfy.org>
2016-03-03 18:48:16 -0800

    Requirements item 4

--- Interests/interests.rb
+++ Interests/interests.rb
@@ -5,19 +5,41 @@ require 'tilt/erubis'
 
 LIST_OF_NAMES = 'public/users.yaml'
 
+before do
+  @info = Psych.load_file LIST_OF_NAMES
+end
+
 get '/' do
   redirect '/names'
 end
 
 get '/names' do
   @title = 'All Names'
-  @names = Psych.load_file LIST_OF_NAMES
 
   erb :names
 end
 
 get '/user' do
   @name = params['name']
+  @title = "#{@name}'s Email and Interests"
+  this_user = @info.find { |item| @name == item.first.to_s }
+  @email = this_user.last[:email]
+  @interests = this_user.last[:interests]
 
   erb :user
+end
+
+helpers do
+  def other_users
+    @info.reject { |item| item.to_s == @name }.map(&:first)
+  end
+
+  def user_as_link(name)
+    <<-END
+      <a href="/user?name=#{name}" class="navigate-to-user"
+         id="navigate-to-user-#{name}">
+        #{name}
+      </a>
+    END
+  end
 end
